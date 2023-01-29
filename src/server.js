c025f82c0b7bba29ae4a20279e50b97a49562138
maeta187 <nexus18789@gmail.com>
2023-01-29 14:47:17 +0900

    ✨ : Queryの実装

--- src/server.js
+++ src/server.js
@@ -1,16 +1,36 @@
 const { ApolloServer, gql } = require('apollo-server')
 
+//HackerNewsのデータ
+let links = [
+  {
+    id: 'link-0',
+    description: 'GraphQLチュートリアル',
+    url: 'www.udemy-graphql-tutorial.cpm'
+  }
+]
+
 // GraphQLのスキーマー
 const typeDefs = gql`
+  type Link {
+    id: ID!
+    description: String!
+    url: String
+  }
+
   type Query {
     info: String!
+    feed: [Link]!
+  }
+
   }
 `
 
 // resolver関数
 const resolvers = {
   Query: {
-    info: () => 'HackerNewsクローン'
+    info: () => 'HackerNewsクローン',
+    feed: () => links
+  },
   }
 }
 
