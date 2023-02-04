4a69bf7e9725ea81787efd14ce7ab90385529fc7
maeta187 <nexus18789@gmail.com>
2023-02-04 16:02:40 +0900

    ✨ : loginの実装

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -24,3 +24,27 @@ async function signup(parent, args, context) {
   }
 }
 
+// ユーザーログイン
+async function login(parent, args, context) {
+  const user = await context.prisma.user.findUnique({
+    where: { email: args.email }
+  })
+  if (!user) {
+    throw new Error('そのようなユーザーは存在しません')
+  }
+
+  // パスワードの比較
+  const valid = await bcrypt.compare(args.password, user.parent)
+  if (!valid) {
+    throw new Error('無効なパスワード')
+  }
+
+  // パスワードが正しいとき
+  const token = jwt.sign({ userId: user.id }, APP_SECRETS)
+
+  return {
+    token,
+    user
+  }
+}
+
